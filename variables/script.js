8347386663b111ccb95c1b7a80fffb0d960d2472
beisagasaul <beisagasaul21@gmail.com>
2024-11-25 22:04:34 -0700

    variables

---
+++ variables/script.js
@@ -0,0 +1,34 @@
+
+//Variables 
+var nombre="Saul";
+var Nombre="Marcelo";
+var _nombre="Marcos";
+var NOMBRE="Maria";
+var $_nombre="Luis";
+
+let saludo="Hola";
+let Saludo ="hi";
+let _saludo="hello";
+let SALUDO ="Adios";
+let $_saludo="Chau";
+
+
+
+let años=21;
+let _estatura=1.67;
+let esVerdadero=false;
+let esNulo=null;
+let persona={nombre:'Juan', edad:24};
+document.write(saludo);
+document.write(`<br>`);
+document.write(Nombre);
+document.write(`<br>`);
+document.write(años);
+document.write(`<br>`);
+document.write(_estatura);
+document.write(`<br>`);
+document.write(esVerdadero);
+document.write(`<br>`);
+document.write(esNulo);
+document.write(`<br>`);
+document.write(persona.nombre ,persona.edad);
