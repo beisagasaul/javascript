307c9de9010d9cb2e6b16df86bbe40ebc1295f87
beisagasaul <beisagasaul21@gmail.com>
2024-11-28 01:35:07 -0700

    valores,tipos y operadores

--- 1. valores y tipos de operadores/scripts.js
+++ 1. valores y tipos de operadores/scripts.js
@@ -1,3 +1,36 @@
+//Declaración de Variables
+let nombre='Juan';//------>Cambiable
+const estudiante='Saul'; //---->constante
+
+let $_saludo="Chau"; //string  
+let años=21; //number
+let Saludo ;//undefined
+let esVerdadero=false; //boolean
+let _esnulo=null; //null
+
+//Nombres de Variables
+let apellido = "Beisaga";  // Válido
+let _edad = 30;       // Válido
+let $salario = 1000;  // Válido
+
+//Ámbito (Scope)
+//Global: Variables definidas fuera de funciones o bloques están disponibles en todo el código.
+//Función: Variables definidas dentro de una función solo están disponibles dentro de esa función.
+//Bloque: Variables declaradas con let o const dentro de {} solo existen dentro del bloque.
+
+//Inicializacion y tipos
+/*
+String: Cadenas de texto ("Hola", 'Mundo').
+Number: Números (42, 3.14).
+Boolean: Verdadero o falso (true, false).
+Undefined: Variable declarada pero no inicializada.
+Null: Intencionadamente vacía.
+Object: Arreglos, funciones, fechas, etc.
+Symbol: Valores únicos.
+BigInt: Para números muy grandes.
+
+*/
+
 // Modulo
 console.log(12%5);//0
 
